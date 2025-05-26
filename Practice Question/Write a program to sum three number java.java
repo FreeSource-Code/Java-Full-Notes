b624cd5c3f2f6f18c851b66ab294a0964685d0ab
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2025-05-26 22:41:39 +0530

    Create Write a program to sum three number java.java

---
+++ Practice Question/Write a program to sum three number java.java
@@ -0,0 +1,13 @@
+public class day1{
+  public static void main (String[]args)
+  {
+    int a=4;
+    int b=5;
+    int c=17;
+    int sum=a+b+c;
+    System.out.println(sum);
+  }
+}
+
+OutPut: 28
+  
