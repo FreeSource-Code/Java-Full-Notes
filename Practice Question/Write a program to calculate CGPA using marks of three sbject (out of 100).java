45f71fb44fabe7c2b3912d1c5c0e7ab24817878c
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2025-05-27 21:33:33 +0530

    Create Write a program to calculate CGPA using marks of three sbject (out of 100).java

---
+++ Practice Question/Write a program to calculate CGPA using marks of three sbject (out of 100).java
@@ -0,0 +1,10 @@
+public class calculateCGPA {
+public static void main (String[]args)
+  {
+    float subject1=45;
+    float subject2=95;
+    float subject3=48;
+    float cgpa=( subject1 + subject2 + subject3 )/30;
+    System.out.println(cgpa);
+  }
+}
